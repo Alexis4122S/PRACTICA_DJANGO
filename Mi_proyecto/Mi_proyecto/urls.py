7c5a54b858cad74c3f16791dca9e2f02a37dbca6
ALEXIS <cuellaralexis29@gmail.com>
2025-03-29 00:10:32 -0400

    Actualización de archivos y ajustes en el proyecto

--- Mi_proyecto/Mi_proyecto/urls.py
+++ Mi_proyecto/Mi_proyecto/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),  # Redirección sin usar lambda
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
     path('login/', views.login, name='login'),
